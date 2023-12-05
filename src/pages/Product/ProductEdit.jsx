51d9fd8c38798f2ef19d9fd7e8151e517ae29a38
gauga123 <128126797+gauga123@users.noreply.github.com>
2023-12-05 21:08:04 +0700

    2

--- src/pages/Product/ProductEdit.jsx
+++ src/pages/Product/ProductEdit.jsx
@@ -1,3 +1,19 @@
+import { useForm } from "react-hook-form"
+
 export default function Edit() {
-  return <div>Edit</div>;
+  
+
+
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data, e) => console.log(data, e)
+  const onError = (errors, e) => console.log(errors, e)
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
+      <input {...register("firstName")} />
+      <input {...register("lastName")} />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
+
